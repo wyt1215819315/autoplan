b792a59d226a0229a34d9cbc8553e163ad0d5fc6
wyt1215819315 <2676831402@qq.com>
2021-12-31 14:12:10 +0800

    ajax登录支持返回具体错误类型

--- src/main/java/com/oldwu/security/LoginAuthenticationFailureHandler.java
+++ src/main/java/com/oldwu/security/LoginAuthenticationFailureHandler.java
@@ -1,6 +1,8 @@
 package com.oldwu.security;
 
 import com.oldwu.entity.AjaxResult;
+import com.oldwu.security.validate.ValidateCodeException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
@@ -17,10 +19,18 @@ public class LoginAuthenticationFailureHandler extends SimpleUrlAuthenticationFa
 
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-
         response.setContentType("application/json;charset=utf-8");
         PrintWriter out = response.getWriter();
-        out.write(AjaxResult.toJson(AjaxResult.doError(exception.getMessage())));
+
+        //判断错误类型
+        if (exception.getClass() == BadCredentialsException.class){
+            out.write(AjaxResult.toJson(AjaxResult.doError("用户名或密码错误，请检查！")));
+        }else if (exception.getClass() == ValidateCodeException.class){
+            out.write(AjaxResult.toJson(AjaxResult.doError(exception.getMessage())));
+        }else {
+            out.write(AjaxResult.toJson(AjaxResult.doError(exception.getMessage())));
+        }
+
         out.flush();
         out.close();
     }
