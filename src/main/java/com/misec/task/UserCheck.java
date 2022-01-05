2adfe301037b2bb8383cd9875a1916c6f566cd6d
MuXia_Firm <504792896@qq.com>
2022-01-05 10:10:46 +0800

    解决，b站自动任务，登录失败还输出用户信息日志的bug

--- src/main/java/com/misec/task/UserCheck.java
+++ src/main/java/com/misec/task/UserCheck.java
@@ -41,6 +41,8 @@ public class UserCheck implements Task {
                 log.debug(String.valueOf(userJson));
                 OldwuLog.error("Cookies可能失效了,请仔细检查配置中的DEDEUSERID SESSDATA BILI_JCT三项的值是否正确、过期");
                 log.warn("Cookies可能失效了,请仔细检查配置中的DEDEUSERID SESSDATA BILI_JCT三项的值是否正确、过期");
+
+                return;
             }
             OldwuLog.log("硬币余额: " + userInfo.getMoney());
             OldwuLog.log("用户名称: " + HelpUtil.userNameEncode(userInfo.getUname()));
