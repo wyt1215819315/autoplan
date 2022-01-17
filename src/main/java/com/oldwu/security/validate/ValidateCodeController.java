d7f3b9b384b968ea2a4f4dc5309e368420559f1a
MuXia_Firm <504792896@qq.com>
2022-01-17 11:33:25 +0800

    更改验证码位置变化的实现方式

--- src/main/java/com/oldwu/security/validate/ValidateCodeController.java
+++ src/main/java/com/oldwu/security/validate/ValidateCodeController.java
@@ -5,7 +5,6 @@ import org.springframework.social.connect.web.SessionStrategy;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.ServletWebRequest;
-import sun.font.FontDesignMetrics;
 
 import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
@@ -52,37 +51,32 @@ public class ValidateCodeController {
 
         Random random = new Random();
 
-        Font font = new Font("Times New Roman", Font.ITALIC, 30);
+        int fontSize = height - 4;
+
         //设置颜色、并随机绘制直线
         g.setColor(getRandColor(200, 250));
         g.fillRect(0, 0, width, height);
-        g.setFont(font);
+        g.setFont(new Font("Times New Roman", Font.ITALIC, fontSize));
         g.setColor(getRandColor(160, 200));
         for (int i = 0; i < 155; i++) {
             int x = random.nextInt(width);
             int y = random.nextInt(height);
-            int xl = random.nextInt(12);
-            int yl = random.nextInt(12);
+            int xl = random.nextInt(16);
+            int yl = random.nextInt(16);
             g.drawLine(x, y, x + xl, y + yl);
         }
 
         //生成随机数 并绘制
         StringBuilder sRand = new StringBuilder();
+
         for (int i = 0; i < length; i++) {
-            String rand = String.valueOf(random.nextInt(10));
-            sRand.append(rand);
             g.setColor(new Color(20 + random.nextInt(110), 20 + random.nextInt(110), 20 + random.nextInt(110)));
 
-            FontDesignMetrics metrics = FontDesignMetrics.getMetrics(font);
-            int strHeight = metrics.getHeight();
-            //顶边位置+上升距离（原本字体基线位置对准画布的y坐标导致字体偏上ascent距离，加上ascent后下移刚好顶边吻合）
-            int top = (height - strHeight) / 2 + metrics.getAscent();
+            String rand = String.valueOf(random.nextInt(10));
+            sRand.append(rand);
 
-            if (i % 2 == 0) {
-                g.drawString(rand, 18 * i + 14, top + i%3);
-            } else {
-                g.drawString(rand, 18 * i + 14, top - i%3);
-            }
+            g.translate(random.nextInt(3), random.nextInt(3));
+            g.drawString(rand, ((width - 10) / length) * i + 5, height / 2 + fontSize / 2 - 8);
 
         }
         g.dispose();
