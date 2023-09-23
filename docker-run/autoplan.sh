6d827b191f61c505bfb1955ac101e391b8247071
MuXia-0326 <504792896@qq.com>
2023-09-24 00:21:26 +0800

    修改docker启动脚本

--- docker-run/autoplan.sh
+++ docker-run/autoplan.sh
@@ -1,14 +1,23 @@
+#!bin/bash
+
+# 输入数据库的密码
+MYSQL_PASSWORD = ""
+
 docker network create autoplan_net
 
 docker run --name autoplan-mysql \
 -p 3306:3306 \
--e MYSQL_ROOT_PASSWORD="密码" \
+-e MYSQL_ROOT_PASSWORD=$MYSQL_PASSWORD \
 -v ./auto_plan_docker.sql:/docker-entrypoint-initdb.d/auto_plan_docker.sql \
 --network autoplan_net --network-alias mysql \
 --restart=always \
 -d mysql:8.0.19
 
-sleep 5
+# 等待 MySQL 容器启动完毕
+echo "等待 MySQL 容器启动..."
+while ! docker exec autoplan-mysql mysqladmin ping -u root --password=$MYSQL_PASSWORD --host=mysql --silent; do
+    sleep 1
+done
 
 docker run --name autoplan \
 -p 26666:26666 \
