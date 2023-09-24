9aeefc43e748f0385662015d50bde8982921d9a4
oldwu <2676831402@qq.com>
2023-09-24 21:44:16 +0800

    feat http工具类封装代理功能，推送代理支持

--- src/main/java/com/github/push/base/service/PushService.java
+++ src/main/java/com/github/push/base/service/PushService.java
@@ -5,6 +5,7 @@ import com.github.push.base.dto.PushResultDto;
 import com.github.push.base.exception.PushRequestException;
 import com.github.push.base.model.PushBaseConfig;
 import com.github.push.base.model.PushData;
+import com.github.system.configuration.ProxyChildConfiguration;
 import com.github.system.util.HttpUtil;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -25,15 +26,25 @@ public interface PushService<T extends PushBaseConfig> {
 
     default JSONObject request(String url, Map<String, Object> params, HttpUtil.RequestType requestType) throws PushRequestException {
         try {
-            JSONObject jsonObject = HttpUtil.requestRetryJson(url, params, requestType);
+            boolean useProxy = isUseProxy();
+            JSONObject jsonObject = HttpUtil.requestRetryJson(url, params, requestType, useProxy);
             if (jsonObject.isEmpty()) {
                 throw new PushRequestException("推送内部错误（返回结果集为空！）");
             }
             return jsonObject;
         } catch (Exception e) {
-            logger.error("推送内部错误",e);
+            logger.error("推送内部错误", e);
             throw new PushRequestException("推送内部错误：" + e.getMessage());
         }
+    }
+
+    default boolean isUseProxy() {
+        boolean useProxy = false;
+        ProxyChildConfiguration pushConfig = HttpUtil.proxyConfiguration.getPushConfig();
+        if (pushConfig.isEnable() && pushConfig.getUse().contains(this.getName())) {
+            useProxy = true;
+        }
+        return useProxy;
     }
 
 }
--- src/main/java/com/github/system/util/HttpUtil.java
+++ src/main/java/com/github/system/util/HttpUtil.java
@@ -2,12 +2,19 @@ package com.github.system.util;
 
 
 import cn.hutool.core.util.CharsetUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.http.HttpRequest;
 import cn.hutool.http.HttpResponse;
 import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 import com.github.rholder.retry.*;
+import com.github.system.configuration.ProxyConfiguration;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import java.net.InetSocketAddress;
+import java.net.Proxy;
 import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.Callable;
@@ -18,14 +25,64 @@ import java.util.concurrent.TimeUnit;
  * http请求工具类
  */
 @Slf4j
+@Component
 public class HttpUtil extends cn.hutool.http.HttpUtil {
     private final static int defaultTimeOut = 5;
     private final static int defaultRetryCount = 2;
     private final static int defaultRetryWaitTime = 2;
+    public static ProxyConfiguration proxyConfiguration;
     //定义重试机制
 
     public enum RequestType {
         GET, JSON, FormData, X_WWW_FORM
+    }
+
+    @Autowired
+    public void setProxyConfiguration(ProxyConfiguration proxyConfiguration) {
+        HttpUtil.proxyConfiguration = proxyConfiguration;
+    }
+
+    /**
+     * 发起请求返回jsonObj 不带重试机制 默认超时
+     *
+     * @param url         请求url
+     * @param params      请求内容
+     * @param headers     请求头Map
+     * @param requestType 请求类型
+     * @param useProxy    是否使用代理
+     * @return jsonObj
+     */
+    public static JSONObject requestJson(String url, Map<String, Object> params, Map<String, String> headers, RequestType requestType, boolean useProxy) {
+        HttpResponse response = request(url, params, headers, requestType, useProxy);
+        JSONObject jsonObject = JSONUtil.parseObj(response.body());
+        response.close();
+        return jsonObject;
+    }
+
+    /**
+     * 发起请求返回jsonObj 不带重试机制 默认超时
+     *
+     * @param url         请求url
+     * @param params      请求内容
+     * @param headers     请求头Map
+     * @param requestType 请求类型
+     * @return jsonObj
+     */
+    public static JSONObject requestJson(String url, Map<String, Object> params, Map<String, String> headers, RequestType requestType) {
+        return requestJson(url, params, headers, requestType, false);
+    }
+
+    /**
+     * 发起请求返回jsonObj 不带重试机制 默认超时
+     *
+     * @param url         请求url
+     * @param params      请求内容
+     * @param requestType 请求类型
+     * @param useProxy    是否使用代理
+     * @return jsonObj
+     */
+    public static JSONObject requestJson(String url, Map<String, Object> params, RequestType requestType, boolean useProxy) {
+        return requestJson(url, params, null, requestType, useProxy);
     }
 
     /**
@@ -37,10 +94,34 @@ public class HttpUtil extends cn.hutool.http.HttpUtil {
      * @return jsonObj
      */
     public static JSONObject requestJson(String url, Map<String, Object> params, RequestType requestType) {
-        HttpResponse response = request(url, params, requestType);
-        JSONObject jsonObject = JSONUtil.parseObj(response.body());
-        response.close();
-        return jsonObject;
+        return requestJson(url, params, null, requestType);
+    }
+
+    /**
+     * 发起请求 不带重试机制 默认超时
+     *
+     * @param url         请求url
+     * @param params      请求内容
+     * @param headers     请求头Map
+     * @param requestType 请求类型
+     * @param useProxy    是否使用代理
+     * @return httpResponse
+     */
+    public static HttpResponse request(String url, Map<String, Object> params, Map<String, String> headers, RequestType requestType, boolean useProxy) {
+        return request(url, params, headers, requestType, defaultTimeOut, useProxy);
+    }
+
+    /**
+     * 发起请求 不带重试机制 默认超时
+     *
+     * @param url         请求url
+     * @param params      请求内容
+     * @param headers     请求头Map
+     * @param requestType 请求类型
+     * @return httpResponse
+     */
+    public static HttpResponse request(String url, Map<String, Object> params, Map<String, String> headers, RequestType requestType) {
+        return request(url, params, headers, requestType, false);
     }
 
     /**
@@ -52,7 +133,34 @@ public class HttpUtil extends cn.hutool.http.HttpUtil {
      * @return httpResponse
      */
     public static HttpResponse request(String url, Map<String, Object> params, RequestType requestType) {
-        return request(url, params, requestType, defaultTimeOut);
+        return request(url, params, null, requestType);
+    }
+
+    /**
+     * 使用默认重试机制请求
+     *
+     * @param url         请求url
+     * @param params      请求内容
+     * @param headers     请求头Map
+     * @param requestType 请求类型
+     * @param useProxy    是否使用代理请求
+     * @return httpResponse
+     */
+    public static HttpResponse requestRetry(String url, Map<String, Object> params, Map<String, String> headers, RequestType requestType, boolean useProxy) {
+        return requestRetry(url, params, headers, requestType, defaultTimeOut, useProxy, defaultRetryCount, defaultRetryWaitTime);
+    }
+
+    /**
+     * 使用默认重试机制请求
+     *
+     * @param url         请求url
+     * @param params      请求内容
+     * @param headers     请求头Map
+     * @param requestType 请求类型
+     * @return httpResponse
+     */
+    public static HttpResponse requestRetry(String url, Map<String, Object> params, Map<String, String> headers, RequestType requestType) {
+        return requestRetry(url, params, headers, requestType, false);
     }
 
     /**
@@ -64,7 +172,50 @@ public class HttpUtil extends cn.hutool.http.HttpUtil {
      * @return httpResponse
      */
     public static HttpResponse requestRetry(String url, Map<String, Object> params, RequestType requestType) {
-        return requestRetry(url, params, requestType, defaultTimeOut, defaultRetryCount, defaultRetryWaitTime);
+        return requestRetry(url, params, null, requestType);
+    }
+
+    /**
+     * 使用默认重试机制请求 返回jsonObj
+     *
+     * @param url         请求url
+     * @param params      请求内容
+     * @param headers     请求头Map
+     * @param requestType 请求类型
+     * @param useProxy    是否使用代理请求
+     * @return jsonObj
+     */
+    public static JSONObject requestRetryJson(String url, Map<String, Object> params, Map<String, String> headers, RequestType requestType, boolean useProxy) {
+        HttpResponse response = requestRetry(url, params, headers, requestType, useProxy);
+        JSONObject jsonObject = JSONUtil.parseObj(response.body());
+        response.close();
+        return jsonObject;
+    }
+
+    /**
+     * 使用默认重试机制请求 返回jsonObj
+     *
+     * @param url         请求url
+     * @param params      请求内容
+     * @param headers     请求头Map
+     * @param requestType 请求类型
+     * @return jsonObj
+     */
+    public static JSONObject requestRetryJson(String url, Map<String, Object> params, Map<String, String> headers, RequestType requestType) {
+        return requestRetryJson(url, params, headers, requestType, false);
+    }
+
+    /**
+     * 使用默认重试机制请求 返回jsonObj
+     *
+     * @param url         请求url
+     * @param params      请求内容
+     * @param requestType 请求类型
+     * @param useProxy    是否使用代理
+     * @return jsonObj
+     */
+    public static JSONObject requestRetryJson(String url, Map<String, Object> params, RequestType requestType, boolean useProxy) {
+        return requestRetryJson(url, params, null, requestType, useProxy);
     }
 
     /**
@@ -76,10 +227,7 @@ public class HttpUtil extends cn.hutool.http.HttpUtil {
      * @return jsonObj
      */
     public static JSONObject requestRetryJson(String url, Map<String, Object> params, RequestType requestType) {
-        HttpResponse response = requestRetry(url, params, requestType);
-        JSONObject jsonObject = JSONUtil.parseObj(response.body());
-        response.close();
-        return jsonObject;
+        return requestRetryJson(url, params, null, requestType);
     }
 
     /**
@@ -87,13 +235,15 @@ public class HttpUtil extends cn.hutool.http.HttpUtil {
      *
      * @param url         请求url
      * @param params      请求内容
+     * @param headers     请求头Map
      * @param requestType 请求类型
      * @param timeout     超时时间
+     * @param useProxy    是否使用代理
      * @param retryCount  重试次数
      * @param waitTime    等待时间
      * @return httpResponse
      */
-    public static HttpResponse requestRetry(String url, Map<String, Object> params, RequestType requestType, int timeout, int retryCount, int waitTime) {
+    public static HttpResponse requestRetry(String url, Map<String, Object> params, Map<String, String> headers, RequestType requestType, int timeout, boolean useProxy, int retryCount, int waitTime) {
         Retryer<HttpResponse> retryer = RetryerBuilder.<HttpResponse>newBuilder()
                 .retryIfException()
                 .retryIfRuntimeException()
@@ -108,48 +258,41 @@ public class HttpUtil extends cn.hutool.http.HttpUtil {
                 .withAttemptTimeLimiter(AttemptTimeLimiters.fixedTimeLimit(timeout, TimeUnit.SECONDS))
                 .build();
         try {
-            return retryCall(url, params, requestType, timeout, retryer);
+            return retryCall(url, params, headers, requestType, timeout, useProxy, retryer);
         } catch (ExecutionException | RetryException e) {
             log.error("重试最终失败{},url={}", e.getMessage(), url);
             return null;
         }
     }
 
-    public static HttpResponse request(String url, Map<String, Object> params, RequestType requestType, int timeout) {
+    public static HttpResponse request(String url, Map<String, Object> params, Map<String, String> headers, RequestType requestType, int timeout, boolean useProxy) {
+        HttpRequest httpRequest;
         switch (requestType) {
-            case GET -> {
-                return createGet(url)
-                        .form(params)
-                        .timeout(timeout).execute();
-            }
-            case JSON -> {
-                String jsonStr = JSONUtil.toJsonStr(params);
-                return createPost(url)
-                        .body(jsonStr)
-                        .timeout(timeout).execute();
-            }
-            case FormData -> {
-                return createPost(url)
-                        .form(params)
-                        .timeout(timeout).execute();
-            }
-            case X_WWW_FORM -> {
-
-                return createPost(url)
-                        .body(toParams(params, CharsetUtil.CHARSET_UTF_8, true))
-                        .timeout(timeout).execute();
-            }
+            case GET -> httpRequest = createGet(url).form(params);
+            case JSON -> httpRequest = createPost(url).body(JSONUtil.toJsonStr(params));
+            case FormData -> httpRequest = createPost(url).form(params);
+            case X_WWW_FORM -> httpRequest = createPost(url).body(toParams(params, CharsetUtil.CHARSET_UTF_8, true));
             default -> {
                 log.warn("未传入请求方式，默认使用Post Json进行请求！");
                 String jsonStr = JSONUtil.toJsonStr(params);
-                return createPost(url)
-                        .body(jsonStr)
-                        .timeout(timeout).execute();
+                httpRequest = createPost(url)
+                        .body(jsonStr);
             }
         }
+        if (useProxy && StrUtil.isNotBlank(proxyConfiguration.getIp()) && proxyConfiguration.getPort() != null) {
+            Proxy.Type proxyType = Proxy.Type.HTTP;
+            if ("socket".equalsIgnoreCase(proxyConfiguration.getType())) {
+                proxyType = Proxy.Type.SOCKS;
+            }
+            httpRequest.setProxy(new Proxy(proxyType, new InetSocketAddress(proxyConfiguration.getIp(), proxyConfiguration.getPort())));
+        }
+        if (headers != null && !headers.isEmpty()) {
+            return httpRequest.headerMap(headers, true).timeout(timeout).execute();
+        }
+        return httpRequest.timeout(timeout).execute();
     }
 
-    private static HttpResponse retryCall(String url, Map<String, Object> params, RequestType requestType, int defaultTimeOut, Retryer<HttpResponse> defaultRetryer) throws ExecutionException, RetryException {
+    private static HttpResponse retryCall(String url, Map<String, Object> params, Map<String, String> headers, RequestType requestType, int timeout, boolean useProxy, Retryer<HttpResponse> defaultRetryer) throws ExecutionException, RetryException {
         Callable<HttpResponse> callable = new Callable<>() {
             int times = 1;
 
@@ -157,7 +300,7 @@ public class HttpUtil extends cn.hutool.http.HttpUtil {
             public HttpResponse call() {
                 log.info("触发重试第{}次，url={}", times, url);
                 times++;
-                return request(url, params, requestType, defaultTimeOut);
+                return request(url, params, headers, requestType, timeout, useProxy);
             }
         };
         return defaultRetryer.call(callable);
