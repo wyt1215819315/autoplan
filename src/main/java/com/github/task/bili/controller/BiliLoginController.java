ca1b8f42455734c5fcf1f627dff7228d78c7ba87
oldwu <2676831402@qq.com>
2023-11-23 23:51:20 +0800

    feat 关闭资源

--- src/main/java/com/github/task/bili/controller/BiliLoginController.java
+++ src/main/java/com/github/task/bili/controller/BiliLoginController.java
@@ -1,5 +1,6 @@
 package com.github.task.bili.controller;
 
+import cn.hutool.core.io.IoUtil;
 import cn.hutool.http.HttpResponse;
 import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
@@ -22,24 +23,29 @@ public class BiliLoginController {
 
     @GetMapping("/getQrCodeResult/{key}")
     public AjaxResult getQrCodeResult(@PathVariable String key) {
-        HttpResponse execute = HttpUtil.createGet(BiliUrlConstant.NEW_BILI_QRCODE_STATUS_URL + key).execute();
-        if (execute.isOk()) {
-            JSONObject body = JSONUtil.parseObj(execute.body());
-            if (body.getInt("code") != null && body.getInt("code") == 0) {
-                JSONObject data = body.getJSONObject("data");
-                if (data.getInt("code") == 0) {
-                    // 获取cookie
-                    String[] cookieKey = {"DedeUserID", "DedeUserID__ckMd5", "SESSDATA", "bili_jct"};
-                    for (String ck : cookieKey) {
-                        data.set(ck, execute.getCookieValue(ck));
+        HttpResponse execute = null;
+        try {
+            execute = HttpUtil.createGet(BiliUrlConstant.NEW_BILI_QRCODE_STATUS_URL + key).execute();
+            if (execute.isOk()) {
+                JSONObject body = JSONUtil.parseObj(execute.body());
+                if (body.getInt("code") != null && body.getInt("code") == 0) {
+                    JSONObject data = body.getJSONObject("data");
+                    if (data.getInt("code") == 0) {
+                        // 获取cookie
+                        String[] cookieKey = {"DedeUserID", "DedeUserID__ckMd5", "SESSDATA", "bili_jct"};
+                        for (String ck : cookieKey) {
+                            data.set(ck, execute.getCookieValue(ck));
+                        }
                     }
+                    return AjaxResult.doSuccess(data);
+                } else {
+                    return AjaxResult.doError(body.getStr("message"));
                 }
-                return AjaxResult.doSuccess(data);
-            } else {
-                return AjaxResult.doError(body.getStr("message"));
             }
+            return AjaxResult.doError("检查扫码状态错误，服务器返回code:" + execute.getStatus());
+        } finally {
+            IoUtil.closeIfPosible(execute);
         }
-        return AjaxResult.doError("检查扫码状态错误，服务器返回code:" + execute.getStatus());
     }
 
 
