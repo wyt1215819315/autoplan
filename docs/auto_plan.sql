e80da76391db7180173df6d1623b1a91c9eadfe1
oldwu <2676831402@qq.com>
2023-12-17 21:08:35 +0800

    feat update

--- docs/auto_plan.sql
+++ docs/auto_plan.sql
@@ -81,7 +81,8 @@ CREATE TABLE `log_push_result`  (
                                     `success` int NULL DEFAULT NULL COMMENT '是否成功 0失败 1成功',
                                     `data` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '推送结果数据',
                                     PRIMARY KEY (`id`) USING BTREE,
-                                    INDEX `IDX_LOG_RES_USERID`(`user_id`) USING BTREE
+                                    INDEX `IDX_LOG_RES_USERID`(`user_id`) USING BTREE,
+                                    INDEX `IDX_LOG_RES_TASKID`(`task_id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
