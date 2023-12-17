3fadc6bd5599d43649fb86a3d042ca5b37670ee1
oldwu <2676831402@qq.com>
2023-12-17 21:26:56 +0800

    feat 脚本修改

--- script/dockerrun.sh
+++ script/dockerrun.sh
@@ -6,13 +6,13 @@ docker run --name autoplan \
 -e APP_DB_USER=数据库用户  \
 -e APP_DB_PWD=数据库密码  \
 -v 数据库挂载位置:/app/db \
--d xxxxx
+-d wyt1215819315/auto_plan
 
 # sqlite
 docker run --name autoplan \
 -p 需要映射的端口:80 \
 -v 数据库挂载位置:/app/db \
--d xxxxx
+-d wyt1215819315/auto_plan
 
 # mysql
 docker run --name autoplan \
@@ -21,4 +21,4 @@ docker run --name autoplan \
 -e APP_DB_URL=127.0.0.1:3306/auto_plan  \
 -e APP_DB_USER=root  \
 -e APP_DB_PWD=123456  \
--d xxxxx
+-d wyt1215819315/auto_plan
